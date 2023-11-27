8e7895d74649ada88b8beb02f0006c9bdb29a5f0
hyowon <phwworld@intense.co.kr>
2023-11-27 15:23:51 +0900

    허쉬

---
+++ hershey/assets/js/ui.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    // brand package
+    const swiper = new Swiper('.package-swiper .swiper', {
+        loop: true,
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+});
