32b737348e2fdb5c6fbfbc306b9a6b078ee97de9
phwworld <phwworld@gmail.com>
2024-10-29 11:08:42 +0900

    수정

--- rope/index.js
+++ rope/index.js
@@ -6,6 +6,6 @@ window.addEventListener('scroll', () => {
   const newTop = parseInt(unit.style.top || 12) + (scrollY - lastScrollY);
   const newLeft = parseInt(unit.style.top || 12) + (scrollY - lastScrollY) + 103;
   unit.style.top = `${newTop}px`;
-  unit.style.left = `${newLeft}px`;
+  // unit.style.left = `${newLeft}px`;
   lastScrollY = scrollY;
 })
